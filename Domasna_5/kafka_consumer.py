99ea2b417acabcf13ef3879780eb56bc0d9b2b1d
VelickovskaViktorija <vvickyy0606@gmail.com>
2025-03-26 21:00:45 +0100

    Update kafka_consumer.py

--- Domasna_5/kafka_consumer.py
+++ Domasna_5/kafka_consumer.py
@@ -1,14 +1,14 @@
 from kafka import KafkaConsumer
 import json
 
-# Kafka consumer setup
-consumer = KafkaConsumer(
-    'stock_data',  # Името на topic-от
-    bootstrap_servers='kafka:9092',  # Адреса на Kafka брокерот
-    auto_offset_reset='earliest',  # Почни од почеток на topic-от
-    value_deserializer=lambda v: json.loads(v.decode('utf-8'))) # Декодирај ги пораките како JSON
 
-# Читај ги пораките од Kafka
+consumer = KafkaConsumer(
+    'stock_data',  
+    bootstrap_servers='kafka:9092',  
+    auto_offset_reset='earliest',  
+    value_deserializer=lambda v: json.loads(v.decode('utf-8'))) 
+
+
 for message in consumer:
     print(f"Received message: {message.value}")
 
